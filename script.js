a229f51c097bace6f7ca5261966991ee3c557ed8
Mudasir Murtaza <109586296+mudasirmurtaza@users.noreply.github.com>
2023-05-19 17:54:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-function Solve(val) {
-   var v = document.getElementById('res');
-   v.value += val;
+function clearScreen() {
+	document.getElementById("result").value = "";
 }
-function Result() {
-   var num1 = document.getElementById('res').value;
-   var num2 = eval(num1);
-   document.getElementById('res').value = num2;
+function display(value) {
+	document.getElementById("result").value += value;
 }
-function Clear() {
-   var inp = document.getElementById('res');
-   inp.value = '';
-}
-function Back() {
-   var ev = document.getElementById('res');
-   ev.value = ev.value.slice(0,-1);
+function calculate() {
+	var p = document.getElementById("result").value;
+	var q = eval(p);
+	document.getElementById("result").value = q;
 }
